3915389609ebd9087840c7f7808502b7fb64dabf
Sergei Panfilov <s.panfilov@maximatelecom.ru>
2016-03-09 20:52:40 +0300

    Basic webpack, babel, react starter pack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const PATHS = {
+  app: path.join (__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  entry: {
+    app: PATHS.app
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }
+    ]
+  }
+}

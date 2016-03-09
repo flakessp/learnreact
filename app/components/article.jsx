94784de95357d18610368121da198200f33cfde6
Sergei Panfilov <s.panfilov@maximatelecom.ru>
2016-03-10 01:10:31 +0300

    Конец первого занятия

--- app/components/article.jsx
+++ app/components/article.jsx
@@ -8,27 +8,29 @@ export default class Article extends React.Component {
     }
   };
 
-  render() {
-    return (
-      <div>
-        <h3 onClick = {this.toggleOpen.bind(this)}>{ article.title }</h3>
-        <section>
-          {this.getBody()}
-        </section>
-      </div>
-    );
-  };
-
   getBody(){
     const { article } = this.props;
     const { isOpen } = this.state;
     if (!isOpen) return null;
     return <section>{article.text}</section>
-  }
+  };
 
   toggleOpen() {
     this.setState({
       isOpen: !this.state.isOpen
-    })
-  }
+    });
+  };
+
+  render() {
+    const { article, isSelected, onClick } = this.props;
+    const style = isSelected ? {color: 'red'} : null;
+
+    return (
+      <div style={style}>
+        <h3 onClick = {this.toggleOpen.bind(this)}>{ article.title }</h3>
+        <a href="#" onClick = {onClick}>Select me</a>
+        {this.getBody()}
+      </div>
+    );
+  };
 }
--- app/components/article_list.jsx
+++ app/components/article_list.jsx
@@ -2,10 +2,30 @@ import React from 'react';
 import Article from './article.jsx';
 
 export default class ArticleList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: []
+    }
+  };
+
+  selectArticle(id) {
+    return (ev) =>{
+      if (ev) ev.preventDefault();
+      this.setState({
+        selected: this.state.selected.concat(id)
+      });
+    };
+  }
+
   render() {
     const articleComponents = this.props.articles.map((article) => {
       return <li key = {article.id}>
-        <Article article = {article} />
+        <Article
+          isSelected = {this.state.selected.includes(article.id)}
+          article = {article}
+          onClick={this.selectArticle(article.id).bind(this)}
+        />
       </li>
     });
     return(
@@ -13,9 +33,9 @@ export default class ArticleList extends React.Component {
         {articleComponents}
       </ul>
     );
-  }
+  };
 };
 
 ArticleList.propTypes = {
   articles: React.PropTypes.array.isRequired
-}
+};

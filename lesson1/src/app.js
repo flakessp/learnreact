24d4052f7bddbe424ea268c228c507df84cce229
Sergei Panfilov <s.panfilov@maximatelecom.ru>
2016-03-16 22:09:11 +0300

    Finished with setup

---
+++ lesson1/src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Article from './article.jsx';
+
+ReactDOM.render(<Article />, document.getElementById('app'));
---
+++ lesson1/src/article.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Article extends React.Component {
+  render(){
+    return <h1> Hellow world </h1>
+  }
+}
+
+export default Article;
